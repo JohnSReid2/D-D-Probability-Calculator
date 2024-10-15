c1c5f7937e607ad483b92b73c8728f958f3c604c
John Reid <61147230+JohnSReid2@users.noreply.github.com>
2024-10-15 11:14:37 +0100

    Add files via upload

---
+++ D&D-Propability-Calculator/D&D-Propability-Calculator/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace D_D_Propability_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            trackBar1 = new TrackBar();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(12, 39);
+            trackBar1.Minimum = -10;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(454, 69);
+            trackBar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(478, 544);
+            Controls.Add(trackBar1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TrackBar trackBar1;
+    }
+}
